10e4cf42e292f96702bfd05dacf9caa04292a6ef
Yun Taein <taein2370@naver.com>
2023-11-09 19:39:06 +0900

    UPDATE: 상단 포트폴리오 링크 추가

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links: any[] = []
+  const links = [{ id: 1, name: "Portfolio", to: "https://yuntae.in" }]
   return (
     <StyledWrapper className="">
       <ul>
